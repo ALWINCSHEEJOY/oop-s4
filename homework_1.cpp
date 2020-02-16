bef609d748b0a1084c7eb8573c86c94763d098d6
ALWIN_C <46865870+ALWINCSHEEJOY@users.noreply.github.com>
2020-02-17 01:01:44 +0530

    Add files via upload

---
+++ homework_1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	
+		int a;
+		char c[50];
+		cout<<"enter employee's name";
+		cin.get(c,50);
+		cout<<"enter wage\n";
+		cin>>a;
+		return 0;
+	
+}
